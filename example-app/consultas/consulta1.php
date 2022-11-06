040a0d72803dc686194d0453223a2364b4193de7
LUCAS BETANCOURT <lucas.betancourt@uc.cl>
2022-11-06 16:58:45 -0300

    t

--- example-app/consultas/consulta1.php
+++ example-app/consultas/consulta1.php
@@ -15,8 +15,7 @@
   ?>
 <style>
   table, th, td {border:1px solid black;}
-  th{text-align: center;} 
-  td{text-align: justify;}
+  th, td{text-align: justify;}
   tr:nth-child(even) {background-color: #D6EEEE;}
 </style>
 
