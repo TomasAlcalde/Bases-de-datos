bf724ad0b7b706a68d00136b7ba8d5b76a944e67
TomasAlcalde <talcalden@uc.cl>
2022-11-06 12:23:15 -0300

    Consulta5 v8

--- example-app/consultas/consulta5.php
+++ example-app/consultas/consulta5.php
@@ -6,11 +6,10 @@
   require("../config/conexion.php"); #Llama a conexión, crea el objeto PDO y obtiene la variable $db
 
   $var = $_POST["Evento"];
-  $query = "SELECT Ent.Nombre_evento, SUM(Ing.precio)
+  $query = "SELECT I.Nombre_evento, I.Ingreso FROM (SELECT Ent.Nombre_evento, SUM(Ing.precio) as Ingreso
             FROM Entradas as Ent LEFT OUTER JOIN Precios as Ing
             ON Ent.Nombre_Evento = Ing.Nombre_Evento and Ent.categoria = Ing.categoria and Ent.tipo = Ing.tipo
-            GROUP BY Ent.Nombre_Evento
-            HAVING Ent.Nombre_evento ilike '$var%;";
+            GROUP BY Ent.Nombre_Evento) as I WHERE I.Nombre_Evento ilike '$var'+'%';";
   $result = $db -> prepare($query);
   $result -> execute();
   $dataCollected = $result -> fetchAll(); #Obtiene todos los resultados de la consulta en forma de un arreglo
