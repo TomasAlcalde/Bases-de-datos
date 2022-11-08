07262c227396b03489cf2e983f924eaa4230d082
TomasAlcalde <talcalden@uc.cl>
2022-11-08 19:51:15 -0300

    Columnas

--- example-app/index.php
+++ example-app/index.php
@@ -1,125 +1,149 @@
 <?php include('templates/header.html');   ?>
 
 <body>
+  <div class="row">
+    <div class="col-sm-8">
+      
+      <div class="row bg-secondary text-white">
+        <h2 align="center"> Consultas </h2>
+      </div>
+      <h3 align="center"> ¿Quieres ver el nombre y contacto de las productoras?</h3>
+      
+      <form align="center" action="consultas/consulta1.php" method="post">
+        <br/>
+        <input type="submit" class="btn btn-success" value="Buscar">
+      </form>
+      
+      <br>
+      <br>
+      <br>
 
-<div class="row">
+      <h3 align="center"> ¿Quieres ver cuántos eventos hizo cada productora?</h3>
   
-  <div class="col">.col</div>
-  <div class="col">.col</div>
-</div>
+      <form align="center" action="consultas/consulta2.php" method="post">
+        <br/>
+        <input type="submit" class="btn btn-success" value="Buscar">
+      </form>
+      
+      <br>
+      <br>
+      <br>
 
-  <h3 align="center"> ¿Quieres ver el nombre y contacto de las productoras?</h3>
-
-  <form align="center" action="consultas/consulta1.php" method="post">
-    <br/>
-    <input type="submit" class="btn btn-success" value="Buscar">
-  </form>
+      <h3 align="center"> ¿Quieres ver el último evento de una productora?</h3>
+      
+      <?php
+        #Primero obtenemos todos los tipos de pokemones
+        require("config/conexion.php");
+        $result = $db -> prepare("SELECT * FROM Productoras ORDER BY Nombre;");
+        $result -> execute();
+        $dataCollected = $result -> fetchAll();
+      ?>
   
-  <br>
-  <br>
-  <br>
+      <form align="center" action="consultas/consulta3.php" method="post">
+        Seleccione una productora:
+        <select name="Productora">
+          <?php
+            #Para cada tipo agregamos el tag <option value=value_of_param> visible_value </option>
+            foreach ($dataCollected as $d) {
+              echo "<option value=$d[1]>$d[1]</option>";
+            }
+          ?>
+        </select>
   
-
-  <h3 align="center"> ¿Quieres ver cuántos eventos hizo cada productora?</h3>
+        <br>
+        <br>
   
-  <form align="center" action="consultas/consulta2.php" method="post">
-    <br/>
-    <input type="submit" class="btn btn-success" value="Buscar">
-  </form>
+        <input type="submit" class="btn btn-success" value="Buscar">
+      </form>
+      
+      <br>
+      <br>
+      <br>
   
-  <br>
-  <br>
-  <br>
+      <h3 align="center"> ¿Quieres ver los artistas que han trabajado con una productora?</h3>
+    
+      <form align="center" action="consultas/consulta4.php" method="post">
+        Seleccione una productora:
+        <select name="Productora">
+          <?php
+            #Para cada tipo agregamos el tag <option value=value_of_param> visible_value </option>
+            foreach ($dataCollected as $d) {
+              echo "<option value=$d[1]>$d[1]</option>";
+            }
+          ?>
+        </select>
+            
+        <br>
+        <br>
+        
+        <input type="submit" class="btn btn-success" value="Buscar">
+      </form>
+    
+      <br>
+      <br>
+      <br>
+    
+      <h3 align="center"> ¿Ver cuántos fueron los ingresos de un evento?</h3>
+      
+      <?php
+        #Primero obtenemos todos los tipos de pokemones
+        require("config/conexion.php");
+        $result = $db -> prepare("SELECT * FROM Eventos ORDER BY Nombre;");
+        $result -> execute();
+        $dataCollected = $result -> fetchAll();
+      ?>
+    
+      <form align="center" action="consultas/consulta5.php" method="post">
+        Seleccione una productora:
+        <select name="Evento">
+          <?php
+            #Para cada tipo agregamos el tag <option value=value_of_param> visible_value </option>
+            foreach ($dataCollected as $d) {
+              echo "<option value=$d[1]>$d[1]</option>";
+            }
+          ?>
+        </select>
+      
+        <br>
+        <br>
+        
+        <input type="submit" class="btn btn-success" value="Buscar">
+      </form>
 
-  <h3 align="center"> ¿Quieres ver el último evento de una productora?</h3>
+    
+      <br>
+      <br>
+      <br>
+      
+      <h3 align="center"> ¿Quieres ver cuántos artistas se presentarán en cada evento?</h3>
+    
+      <form align="center" action="consultas/consulta6.php" method="post">
+        <br/>
+        <input type="submit" class="btn btn-success" value="Buscar">
+      </form>
+      
+    
+      <br>
+      <br>
+      <br>
+      
+      <h3 align="center"> ¿Quieres ver el evento con más entradas vendidas?</h3>
+      
+      <form align="center" action="consultas/consulta7.php" method="post">
+        <br/>
+        <input type="submit" class="btn btn-success" value="Buscar">
+      </form>
+      
+      <br>
+      <br>
+      <br>
+    </div>
 
-  <?php
-  #Primero obtenemos todos los tipos de pokemones
-  require("config/conexion.php");
-  $result = $db -> prepare("SELECT * FROM Productoras ORDER BY Nombre;");
-  $result -> execute();
-  $dataCollected = $result -> fetchAll();
-  ?>
-    <form align="center" action="consultas/consulta3.php" method="post">
-      Seleccione una productora:
-      <select name="Productora">
-        <?php
-        #Para cada tipo agregamos el tag <option value=value_of_param> visible_value </option>
-        foreach ($dataCollected as $d) {
-          echo "<option value=$d[1]>$d[1]</option>";
-        }
-        ?>
-      </select>
-      <br><br>
-      <input type="submit" class="btn btn-success" value="Buscar">
-    </form>
-  <br>
-  <br>
-  <br>
-
-  <h3 align="center"> ¿Quieres ver los artistas que han trabajado con una productora?</h3>
-
-    <form align="center" action="consultas/consulta4.php" method="post">
-      Seleccione una productora:
-      <select name="Productora">
-        <?php
-        #Para cada tipo agregamos el tag <option value=value_of_param> visible_value </option>
-        foreach ($dataCollected as $d) {
-          echo "<option value=$d[1]>$d[1]</option>";
-        }
-        ?>
-      </select>
-      <br><br>
-      <input type="submit" class="btn btn-success" value="Buscar">
-    </form>
-  <br>
-  <br>
-  <br>
-
-  <h3 align="center"> ¿Ver cuántos fueron los ingresos de un evento?</h3>
-
-  <?php
-  #Primero obtenemos todos los tipos de pokemones
-  require("config/conexion.php");
-  $result = $db -> prepare("SELECT * FROM Eventos ORDER BY Nombre;");
-  $result -> execute();
-  $dataCollected = $result -> fetchAll();
-  ?>
-    <form align="center" action="consultas/consulta5.php" method="post">
-      Seleccione una productora:
-      <select name="Evento">
-        <?php
-        #Para cada tipo agregamos el tag <option value=value_of_param> visible_value </option>
-        foreach ($dataCollected as $d) {
-          echo "<option value=$d[1]>$d[1]</option>";
-        }
-        ?>
-      </select>
-      <br><br>
-      <input type="submit" class="btn btn-success" value="Buscar">
-    </form>
-  <br>
-  <br>
-  <br>
-
-  <h3 align="center"> ¿Quieres ver cuántos artistas se presentarán en cada evento?</h3>
+    <div class="col-sm-8"> 
+      <div class="row bg-secondary text-white">
+        <h2 align="center"> Datos </h2>
+      </div>
+    </div>
   
-  <form align="center" action="consultas/consulta6.php" method="post">
-    <br/>
-    <input type="submit" class="btn btn-success" value="Buscar">
-  </form>
-  
-  <br>
-  <br>
-  <br>
-  
-  <h3 align="center"> ¿Quieres ver el evento con más entradas vendidas?</h3>
-  
-  <form align="center" action="consultas/consulta7.php" method="post">
-    <br/>
-    <input type="submit" class="btn btn-success" value="Buscar">
-  </form>
-  
-  <br>
-  <br>
-  <br>
+  </div>
+</body>
