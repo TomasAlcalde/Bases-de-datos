6b47755d41cc213fa65317166d811634bcd59110
TomasAlcalde <talcalden@uc.cl>
2022-11-09 19:34:20 -0300

    Center

--- example-app/index.php
+++ example-app/index.php
@@ -11,7 +11,9 @@
 
       <div class="container">
 
-        
+        <br>
+        <br>
+
         <h3 align="center"> ¿Quieres ver el nombre y contacto de las productoras?</h3>
       
         <form align="center" action="consultas/consulta1.php" method="post">
@@ -45,17 +47,19 @@
         ?>
   
         <form align="center" action="consultas/consulta3.php" method="post">
+          
+          <div align="center">
+            <select class="form-select" name="Productora" style="width:400px">
+              <option selected>Seleccione una productora</option>
+              <?php
+                #Para cada tipo agregamos el tag <option value=value_of_param> visible_value </option>
+                foreach ($dataCollected as $d) {
+                  echo "<option value=$d[1]>$d[1]</option>";
+                }
+              ?>
+            </select>
+          </div>
 
-          <select class="form-select" name="Productora" style="width:400px" align="center">
-            <option selected>Seleccione una productora</option>
-            <?php
-              #Para cada tipo agregamos el tag <option value=value_of_param> visible_value </option>
-              foreach ($dataCollected as $d) {
-                echo "<option value=$d[1]>$d[1]</option>";
-              }
-            ?>
-          </select>
-  
           <br>
           <br>
   
@@ -70,15 +74,17 @@
     
         <form align="center" action="consultas/consulta4.php" method="post">
           
-          <select class="form-select" name="Productora" style="width:400px" align="center">
-            <option selected>Seleccione una productora</option>
-            <?php
-              #Para cada tipo agregamos el tag <option value=value_of_param> visible_value </option>
-              foreach ($dataCollected as $d) {
-                echo "<option value=$d[1]>$d[1]</option>";
-              }
-            ?>
-          </select>
+          <div align="center">
+            <select class="form-select" name="Productora" style="width:400px" align="center">
+              <option selected>Seleccione una productora</option>
+              <?php
+                #Para cada tipo agregamos el tag <option value=value_of_param> visible_value </option>
+                foreach ($dataCollected as $d) {
+                  echo "<option value=$d[1]>$d[1]</option>";
+                }
+              ?>
+            </select>
+          </div>
             
           <br>
           <br>
@@ -99,19 +105,21 @@
           $result -> execute();
           $dataCollected = $result -> fetchAll();
         ?>
-    
+        
         <form align="center" action="consultas/consulta5.php" method="post">
         
-          <select class="form-select" name="Evento" style="width:400px" align="center">
-            <option selected>Seleccione un evento</option>
-            <?php
-              #Para cada tipo agregamos el tag <option value=value_of_param> visible_value </option>
-              foreach ($dataCollected as $d) {
-                echo "<option value=$d[1]>$d[1]</option>";
-              }
-            ?>
-          </select>
-      
+          <div align="center">
+            <select class="form-select" name="Evento" style="width:400px" align="center">
+              <option selected>Seleccione un evento</option>
+              <?php
+                #Para cada tipo agregamos el tag <option value=value_of_param> visible_value </option>
+                foreach ($dataCollected as $d) {
+                  echo "<option value=$d[1]>$d[1]</option>";
+                }
+              ?>
+            </select>
+          </div>
+
           <br>
           <br>
         
@@ -135,7 +143,7 @@
         <br>
         <br>
       
-        <h3> ¿Quieres ver el evento con más entradas vendidas?</h3>
+        <h3 align="center"> ¿Quieres ver el evento con más entradas vendidas?</h3>
       
         <form align="center" action="consultas/consulta7.php" method="post">
           <br/>
